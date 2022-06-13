870359d5e74da27a296c6327ad4d8b297d874f19
Ashay Wakode <ashaywakode@gmail.com>
2022-06-13 19:53:22 +0530

    Update and rename mi_gradient_new_formulation.m to tsv_gradient.m

--- tsv_gradient.m
+++ tsv_gradient.m
@@ -1,4 +1,4 @@
-function m = mi_gradient_new_formulation(r,q,delta_q ,n)
+function m = tsv_gradient(r,q,delta_q ,n)
     m = zeros(n,1) ;
     for i = 1:n
         a=tsv(r,q+delta_q*([zeros(1,i-1),1,zeros(1,n-i)])) ;
